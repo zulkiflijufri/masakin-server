2b439798a9a845273f6c315d268537068296b92b
zulkiflijufri <zulkiflijufri54@gmail.com>
2020-12-24 16:41:07 +0800

    catch error

--- app/products/controller.js
+++ app/products/controller.js
@@ -1,12 +1,16 @@
 const Product = require("./model");
 
 async function store(req, res, next) {
-  let payload = req.body;
+  try {
+    let payload = req.body;
 
-  let product = new Product(payload);
-  await product.save();
+    let product = new Product(payload);
+    await product.save();
 
-  return res.json(product);
+    return res.json(product);
+  } catch (error) {
+    next(error);
+  }
 }
 
 // export for use in router
