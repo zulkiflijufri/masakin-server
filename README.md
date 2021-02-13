aab0e6226f0c0b899c0c3577915e080ed3982717
zulkiflijufri <zulkiflijufri54@gmail.com>
2021-02-13 17:41:25 +0800

    model user & validation email

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## Create food app API with ExpressJS
 
 > Implementation from learning by doing with ExpressJS :rocket:
+
+Next: page 216
---
+++ app/users/model.js
@@ -0,0 +1,62 @@
+const mongoose = require("mongoose");
+const { model, Schema } = mongoose;
+
+const userSchema = Schema(
+  {
+    full_name: {
+      type: String,
+      required: [true, "Nama harus diisi"],
+      minlength: [3, "Panjang nama minimal 3 karakter"],
+      maxlength: [255, "Panjang nama maksimal 255 karakter"],
+    },
+    customer_id: {
+      type: Number,
+    },
+    email: {
+      type: String,
+      require: [true, "Email harus diisi"],
+      maxlength: [255, "Panjang email maksimal 255 karakter"],
+    },
+    password: {
+      type: String,
+      require: [true, "Password harus diisi"],
+      maxlength: [255, "Panjang email maksimal 255 karakter"],
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    token: [String],
+  },
+  { timestamps: true }
+);
+
+// custom validation email
+userSchema.path("email").validated(
+  (value) => {
+    // regex
+    const EMAIL_RE = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+    return EMAIL_RE.test(value);
+  },
+  (attr) => `${attr.value} harus merupakan email yang valid`
+);
+
+// custiom validation unique email
+userSchema.path("email").validated(
+  async (value) => {
+    try {
+      // (1) find email
+      const count = await this.model("User").count({ email: value });
+
+      // (2)jika `false` maka validasi gagal
+      // jika `true` maka validasi berhasil
+      return !count;
+    } catch (error) {
+      throw error;
+    }
+  },
+  (attr) => `${attr.value} sudah terdaftar`
+);
+
+module.exports = model("User", userSchema);
